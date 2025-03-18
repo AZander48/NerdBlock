44fd937fae6a9d35d019730923b8695e1e0bbb3f
AZander48 <Alexzander4668@gmail.com>
2025-03-17 21:55:56 -0400

    Add Nunjucks

---
+++ frontend/.eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  // Ignore the 'src' folder and only include the necessary files
+  return {
+    dir: {
+      input: "src",      // Input folder
+      includes: "_includes", // Include folder for layouts/partials
+      output: "_site",    // Output folder
+    },
+    // Optionally, add a passthrough copy to selectively copy assets
+    passthroughFileCopy: true, // This is useful for copying static files like images, CSS, etc.
+  };
+};
+  
---
+++ frontend/src/index.md
@@ -0,0 +1,12 @@
+---
+layout: default
+title: Home
+---
+
+{% include "header.njk" %}
+
+<h2>This is my homepage content!</h2>
+
+<p>Welcome to my Eleventy site.</p>
+
+{% include "footer.njk" %}
